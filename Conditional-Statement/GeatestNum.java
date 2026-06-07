bafbe1f7de4942c2af251cb9e452c7fcb15c2518
Manish  Mishra <manishweb9199@gmail.com>
2026-06-07 23:34:21 +0530

    Create GeatestNum.java

---
+++ Conditional-Statement/GeatestNum.java
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <script>
+    console.log("program starts")
+    console.log=+prompt("Greatest of two number");
+     let n1=number(prompt("enter the n1 value"));
+      let n2=number(prompt("enter the n2 value"));
+      if(n1>n2){
+        conole.log(`${n1} is greatest`)
+      }
+        else{
+             console.log(`${n2} is gratest`)
+        }
+             console.log("program ends");
+      
+  </script>
+</head>
+<body>
+  <h1>welcome javascript</h1>
+</body>
+</html>
